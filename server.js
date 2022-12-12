ebe791c091f0a7d11b4c1bbc5685378a898c5109
19722034 <alik.orujev1997@mail.ru>
2022-12-12 15:26:59 +0300

    sprint_1: fix errors

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.use(express.static("dist"));
 
-app.listen(port, () => {
-    console.log(`Server started on port: ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`)
 })
